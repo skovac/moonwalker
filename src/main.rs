c8bffff2113813a0120575febbf00de574055987
Sébastien <sebastien.kovacs.ext@martingmbh.de>
2023-09-12 00:32:55 +0200

    bevy init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use bevy::prelude::*;
+
+
+#[derive(Component)]
+struct Person;
+
+
+#[derive(Component)]
+struct Name(String);
+
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
+}
+
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
 }
