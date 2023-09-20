10df203c069e8f79fb287d4c0b169347759e4aec
Sébastien Kovacs <sebastien@kovacs.tech>
2023-09-20 14:43:30 +0200

    crates.io test

---
+++ src/light.rs
@@ -0,0 +1,36 @@
+use bevy::{pbr::CascadeShadowConfigBuilder, prelude::*};
+
+pub struct Light;
+
+impl Plugin for Light {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_light);
+            //.add_systems(Update, move_camera);
+    }
+}
+
+fn spawn_light(mut commands: Commands) {
+    // directional 'sun' light
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            color: Color::WHITE,
+            ..default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0.0, 0.0, 0.0),
+            rotation: Quat::from_rotation_x(315.0_f32.to_radians()),
+            ..default()
+        },
+        // The default cascade config is designed to handle large scenes.
+        // As this example has a much smaller world, we can tighten the shadow
+        // bounds for better visual quality.
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 4.0,
+            maximum_distance: 10.0,
+            ..default()
+        }
+        .into(),
+        ..default()
+    });
+}
