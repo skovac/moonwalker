d068c146c2390352cbd7127b50ab89382c9b60ca
Sébastien <sebastien.kovacs.ext@martingmbh.de>
2023-09-20 12:07:47 +0200

    switch to dev branch

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,96 @@
-use bevy::prelude::*;
+use std::f32::consts::PI;
 
-
-#[derive(Component)]
-struct Person;
-
-
-#[derive(Component)]
-struct Name(String);
-
+use bevy::{pbr::CascadeShadowConfigBuilder, prelude::*};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, (hello_world, greet_people))
+        .add_systems(Startup, setup)
+        .add_systems(Update, keyboard_input_system)
         .run();
 }
 
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(500.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
 
-fn hello_world() {
-    println!("hello world!");
+    // sphere
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::UVSphere { ..Default::default() })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 1.0, 0.0),
+        ..default()
+    });
+
+    // cube on sphere
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 0.5 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 2.5, 0.0),
+        ..default()
+    });
+
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 0.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(2.0, 18.0, -4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 8.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    // directional 'sun' light
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            color: Color::WHITE,
+            ..default()
+        },
+        transform: Transform {
+            translation: Vec3::new(0.0, 0.0, 0.0),
+            rotation: Quat::from_rotation_x(315.0_f32.to_radians()),
+            ..default()
+        },
+        // The default cascade config is designed to handle large scenes.
+        // As this example has a much smaller world, we can tighten the shadow
+        // bounds for better visual quality.
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 4.0,
+            maximum_distance: 10.0,
+            ..default()
+        }
+        .into(),
+        ..default()
+    });
 }
 
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.pressed(KeyCode::Left) {
+        info!("'Left' currently pressed");
+    }
+    if keyboard_input.pressed(KeyCode::Right) {
+        info!("'Right' currently pressed");
+    }
+    if keyboard_input.pressed(KeyCode::Up) {
+        info!("'Up' currently pressed");
+    }
+    if keyboard_input.pressed(KeyCode::Down) {
+        info!("'Down' currently pressed");
     }
 }
